8a635bb4718d90e7e6aa9f08de6c54cbe6eae7c7
lucasbarbosa0217 <lucasbarbosa0217@gmail.com>
2023-06-28 21:21:09 -0300

    Adicionei a activity Conquista e TabLayout

--- app/src/main/java/com/example/myapplication/Conquista.kt
+++ app/src/main/java/com/example/myapplication/Conquista.kt
@@ -6,31 +6,29 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
+class Conquista : AppCompatActivity() {
     private lateinit var tabLayout: TabLayout
     private lateinit var categoriaTab: TabLayout.Tab
     private lateinit var conquistaTab: TabLayout.Tab
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_conquista)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-
         // Referenciar o TabLayout e as guias
         tabLayout = findViewById(R.id.tabLayout)
         categoriaTab = tabLayout.getTabAt(0)!! // Índice da guia "categoriaTab"
         conquistaTab = tabLayout.getTabAt(1)!! // Índice da guia "conquistaTab"
 
-        // Configurar a ação ao pressionar a guia "conquistaTab"
-        conquistaTab.view.setOnClickListener {
-            val intent = Intent(this, Conquista::class.java)
+        // Configurar a ação ao pressionar a guia "categoriaTab"
+        categoriaTab.view.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish();
         }
 
-        // Desabilitar a guia "categoriaTab" para que não seja clicável
-        categoriaTab.view.isClickable = false
-        categoriaTab.view.isFocusable = false
-        categoriaTab.select()
-    }
-    }
+        // Desabilitar a guia "conquistaTab" para que não seja clicável
+        conquistaTab.view.isClickable = false
+        conquistaTab.view.isFocusable = false
+        conquistaTab.select()
+}
+}
