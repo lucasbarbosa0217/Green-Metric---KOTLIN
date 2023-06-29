4ca2c859ee7ee78b2937d6cb976f7054b25e024d
lucasbarbosa0217 <lucasbarbosa0217@gmail.com>
2023-06-28 21:32:10 -0300

    Consertei problemas de merge

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -3,18 +3,30 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val btn_home = findViewById<ImageView>(R.id.btn_home)
 
         btn_home.setOnClickListener{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
+            finish()
         }
+        val btn_login = findViewById<Button>(R.id.button)
+        btn_login.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
+
     }
 }
